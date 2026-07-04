6a14f00c28d534032ceffa9178e39c9c961b7488
kaoutarhammaoui <kaoutarhammaoui2@gmail.com>
2026-07-04 15:06:24 +0100

    add ghost state

--- store.js
+++ store.js
@@ -1,14 +1,25 @@
 export var state = {
-    pacMan: {
-        row: 11,
-        col: 9,
-        direction: null,
-        moveTimer: 0,
-        moveDelay: 110
+  pacMan: {
+    row: 11,
+    col: 9,
+    direction: null,
+    moveTimer: 0,
+    moveDelay: 110,
+  },
+  ghosts: [
+    {
+      row: 9,
+      col: 7,
+      direction: "left",
+      color: "pink",
+      moveTimer: 0,
+      moveDelay: 150,
     },
-    score: 0,
-    lives: 3,
-    lastTime: 0,
-    deltaTime: 0,
-    status: 'start' 
+  ],
+
+  score: 0,
+  lives: 3,
+  lastTime: 0,
+  deltaTime: 0,
+  status: "start",
 };
